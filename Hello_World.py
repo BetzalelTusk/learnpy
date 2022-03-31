8ed2111caa6ad86384587752ad43a53dd6d910d7
BetzalelTusk <betzaleltusk@gmail.com>
2022-03-31 14:06:32 -0400

    good dtuff

---
+++ Hello_World.py
@@ -0,0 +1,14 @@
+x = 5    #int
+y = 4.20 #float
+z = 2j   #comlex
+
+a = float(x)   #convert int to float
+b =complex(y)  #convert float to comlex
+c = int(z)     #convert complex to int
+
+print (type(a))
+print (type(b))
+print (type(c))
+
+
+
