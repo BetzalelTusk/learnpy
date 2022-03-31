d98141a6e49c97045d865c2f1bc15207173cc06c
BetzalelTusk <betzaleltusk@gmail.com>
2022-03-31 13:26:15 -0400

    new file acts as test for int/float/complex

--- print.py
+++ print.py
@@ -1,4 +1,20 @@
-x = 1 
-while x <= 2**1000:
-    print(x * 2)
-    x = x * 2
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+#convert from int to float:
+a = float(x)
+
+#convert from float to int:
+b = int(y)
+
+#convert from int to complex:
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c)) 
