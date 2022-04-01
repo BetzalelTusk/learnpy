132fec178dddc478d6bb853f79f71aea25a0cb7e
BetzalelTusk <betzaleltusk@gmail.com>
2022-03-31 20:23:39 -0400

    CHANGED THE NAME

--- C* _F*_Converter.py
+++ C* _F*_Converter.py
@@ -2,7 +2,7 @@ temp = input ("The current weather outside is: ")
 conv = input ("C -> F or F -> C? Type either C or F: ").lower
 
 a = (str(temp)).lower  # a = temperature converted into a str
-b = (str(conv)).lower   # b = either Celcius or Farenheight converted into a str
+b = (str(conv)).lower  # b = either Celcius or Farenheight converted into a str
 
 if b == "c":
     print ((temp * 9/5)+32)
